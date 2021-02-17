9fba025faa87dff1fc29395ef2b9147542404444
Armin <fajkan10@gmail.com>
2021-02-17 11:19:54 +0100

    first implementions

--- src/main/java/DAO/CityDaoJDBC.java
+++ src/main/java/DAO/CityDaoJDBC.java
@@ -1,6 +1,44 @@
 package DAO;
 
-public abstract class CityDaoJDBC implements CityDao{
+import model.City;
+
+import java.util.List;
+
+public class CityDaoJDBC implements CityDao{
 
 
+    @Override
+    public City findByID(int id) {
+        return null;
+    }
+
+    @Override
+    public List<City> findByCode(String code) {
+        return null;
+    }
+
+    @Override
+    public List<City> findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<City> findAll() {
+        return null;
+    }
+
+    @Override
+    public City add(City city) {
+        return null;
+    }
+
+    @Override
+    public City update(City city) {
+        return null;
+    }
+
+    @Override
+    public int delete(City city) {
+        return 0;
+    }
 }
