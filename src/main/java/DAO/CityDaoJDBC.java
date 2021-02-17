6371c1064cfbbf991558d06b4ec3b2fce0375d45
Armin <fajkan10@gmail.com>
2021-02-17 11:34:21 +0100

    FindByID

--- src/main/java/DAO/CityDaoJDBC.java
+++ src/main/java/DAO/CityDaoJDBC.java
@@ -1,14 +1,38 @@
 package DAO;
 
 import model.City;
+import java.sql.*;
 
 import java.util.List;
 
 public class CityDaoJDBC implements CityDao{
 
+    public static Connection mySqlConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/world", "root", "1234");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 
     @Override
     public City findByID(int id) {
+        Connection connection = mySqlConnection();
+        String query ="Select id, name from city where id = ?";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1,id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                System.out.println(resultSet.getInt("id") + " - " + resultSet.getString("name"));
+            } else {
+                System.out.println("not found");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
